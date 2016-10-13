8ce1901e8a4fe103d50becc53e82baf63b1dcb28
James <fifteen2010@gmail.com>
2016-10-13 20:19:22 +0800

    write to file with formated data.

--- STPDebugging/python_basis.py
+++ STPDebugging/python_basis.py
@@ -6,6 +6,7 @@ def write_to_file():
     filename = "log\\test_file_to_write.txt"
     with open(filename, 'w') as f:
         print('hi {} {} {}'.format(1, 1.1, 2323), file = f)
+        print('{0.2f} {1.2f}'.format(1.11, 1.1111), file =f)
         f.write("hehe")
 
 
