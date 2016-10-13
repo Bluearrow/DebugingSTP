f23024f67feed30bfbb6da18273962b1f19513e2
James <fifteen2010@gmail.com>
2016-10-13 19:56:22 +0800

    Update.

--- STPDebugging/python_basis.py
+++ STPDebugging/python_basis.py
@@ -3,7 +3,7 @@
 
 # Write to file with print() and write().
 def write_to_file():
-    filename = "test_file_to_write.txt"
+    filename = "log\\test_file_to_write.txt"
     with open(filename, 'w') as f:
         print('hi {} {} {}'.format(1, 1.1, 2323), file = f)
         f.write("hehe")
